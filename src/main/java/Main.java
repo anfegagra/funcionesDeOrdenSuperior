e313b47c6898c3cf55521eb255a80284d67054d0
Andres Felipe García Granados <garandre@colpatria.com>
2019-11-21 20:32:41 -0500

    obtener una lista de números utilizando las funciones BiFunction<T, U, R> y Predicate<T>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main implements InterfaceSuma {
 
@@ -19,13 +24,28 @@ public class Main implements InterfaceSuma {
 
 		// Interfaz funcional Function<T, R>
 		/*
-
 		interface Function<T t, R r> {
 			R apply(T, t)
 		 */
 		Function<String, String> convertirAMayusculas = nombre -> nombre.toUpperCase();
 		main.imprimirNombreEnMayusculaConFunction(convertirAMayusculas, "pipe");
 
+		// Interfaz funciona BiFunction<T, U, R>
+		/*
+		interface BiFunction<T, U, R> {
+			R apply(T t, U u)
+		 */
+
+		// Interfaz funcional Predicate<T>
+		/*
+		interface Predicate<T t> {
+			Boolean test(t)
+		 */
+		BiFunction<List<Integer>, Predicate<Integer>, List<Integer>> filtrar = (lista, predicado) ->
+			lista.stream().filter(numero -> predicado.test(numero)).collect(Collectors.toList());
+		List<Integer> listaNumeros = Arrays.asList(1, 4, -7, 10, -5, 90);
+		System.out.println("Lista obtenida con BiFunction<T, U, R> y Predicate<T>: " +
+			filtrar.apply(listaNumeros, numero -> numero > 0));
 	}
 
 	public int sumar(int a, int b) {
